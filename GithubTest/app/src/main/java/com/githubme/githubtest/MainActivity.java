1a95198f30b3d8460ddfdfcdf063762a508759d1
chairath2532 <viruszajr@gmail.com>
2020-12-05 20:01:44 +0700

    Test project github

---
+++ GithubTest/app/src/main/java/com/githubme/githubtest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.githubme.githubtest;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
